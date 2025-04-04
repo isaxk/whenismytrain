200d471f270e932e4993376c7a77194c4deb70a0
isaxk <76838845+isaxk@users.noreply.github.com>
2025-04-04 18:40:18 +0100

    fix  search on desktop

--- src/lib/components/ui/station-search.svelte
+++ src/lib/components/ui/station-search.svelte
@@ -58,7 +58,7 @@
 	});
 </script>
 
-<Portal>
+<div class="relative w-full">
 	{#if open}
 		<form
 			onsubmit={() => {
@@ -71,81 +71,88 @@
 					}, 200);
 				}
 			}}
-			class="bg-background pt-safe fixed inset-0 z-50 h-full w-full transform-gpu"
+			class="bg-background pt-safe border-border fixed top-0 -right-3 -left-3 z-50 h-full transform-gpu md:absolute md:h-max md:min-w-[400px] md:rounded-lg md:border md:bg-white md:p-3 md:drop-shadow-md"
 			transition:scale|global={{ duration: 200, start: 0.9 }}
 		>
-			<div class="flex items-center gap-2">
+			<div class="flex items-center gap-2 bg-white">
 				<input
 					bind:this={input}
 					placeholder="Search for a train station..."
 					bind:value
 					class="w-full p-4 text-lg"
+					onblur={() => {
+						setTimeout(() => {
+							open = false;
+						}, 200);
+					}}
 				/>
 				<button onclick={() => (open = false)} class="text-foreground-muted px-2">Cancel</button>
 			</div>
-			{#if value.length > 1 && formatted.length > 0}
-				{#each formatted as result, i (results[i].item.crsCode)}
-					{#if results[i].item.crsCode !== hidden}
-						<button
-							type="button"
-							onclick={() => {
-								selected = results[i].item.crsCode;
-								open = false;
-								setTimeout(() => {
-									onselect(results[i].item.crsCode);
-									value = '';
-								}, 200);
-							}}
-							class="border-border flex w-full items-center gap-2 border-b px-4 py-2 text-left text-zinc-800 last:border-none"
-						>
-							<div class="min-w-0 flex-grow">
-								<div class="overflow-hidden text-xl font-medium text-nowrap text-ellipsis">
-									<Highlight value={result.stationName} />
+			<div class="bg-background">
+				{#if value.length > 1 && formatted.length > 0}
+					{#each formatted as result, i (results[i].item.crsCode)}
+						{#if results[i].item.crsCode !== hidden}
+							<button
+								type="button"
+								onclick={() => {
+									selected = results[i].item.crsCode;
+									open = false;
+									setTimeout(() => {
+										onselect(results[i].item.crsCode);
+										value = '';
+									}, 200);
+								}}
+								class="border-border flex w-full items-center gap-2 border-b px-4 py-2 text-left text-zinc-800 last:border-none"
+							>
+								<div class="min-w-0 flex-grow">
+									<div class="overflow-hidden text-xl font-medium text-nowrap text-ellipsis">
+										<Highlight value={result.stationName} />
+									</div>
+									<div class="text-xs">
+										<Highlight value={result.crsCode} />
+									</div>
 								</div>
-								<div class="text-xs">
-									<Highlight value={result.crsCode} />
+								<div class="">
+									<Check size={22} />
 								</div>
-							</div>
-							<div class="">
-								<Check size={22} />
-							</div>
-						</button>
-					{/if}
-				{/each}
-			{:else if geoStations.length > 0}
-				{#each geoStations.slice(0, 5) as geo (geo.crsCode)}
-					{#if geo.crsCode !== hidden}
-						<button
-							onclick={() => {
-								selected = geo.crsCode;
-								open = false;
-								setTimeout(() => {
-									onselect(geo.crsCode);
-									value = '';
-								}, 200);
-							}}
-							type="button"
-							class="border-border flex w-full items-center gap-2 border-b px-4 py-2 text-left text-zinc-800 last:border-none"
-						>
-							<div class="min-w-0 flex-grow">
-								<div class="overflow-hidden text-xl font-medium text-nowrap text-ellipsis">
-									{geo.stationName}
+							</button>
+						{/if}
+					{/each}
+				{:else if geoStations.length > 0}
+					{#each geoStations.slice(0, 5) as geo (geo.crsCode)}
+						{#if geo.crsCode !== hidden}
+							<button
+								onclick={() => {
+									selected = geo.crsCode;
+									open = false;
+									setTimeout(() => {
+										onselect(geo.crsCode);
+										value = '';
+									}, 200);
+								}}
+								type="button"
+								class="border-border flex w-full items-center gap-2 border-b px-4 py-2 text-left text-zinc-800 last:border-none"
+							>
+								<div class="min-w-0 flex-grow">
+									<div class="overflow-hidden text-xl font-medium text-nowrap text-ellipsis">
+										{geo.stationName}
+									</div>
+									<div class="text-xs">
+										{geo.crsCode} - {geo.distance.toFixed(2)}km
+									</div>
 								</div>
-								<div class="text-xs">
-									{geo.crsCode} - {geo.distance.toFixed(2)}km
+								<div class="">
+									<Check size={22} />
 								</div>
-							</div>
-							<div class="">
-								<Check size={22} />
-							</div>
-						</button>
-					{/if}
-				{/each}
-			{/if}
+							</button>
+						{/if}
+					{/each}
+				{/if}
+			</div>
 		</form>
 	{/if}
-</Portal>
 
-<button class={['contents', className]} onclick={() => (open = true)}>
-	{@render children(selectedStation?.stationName ?? null, selectedStation?.crsCode ?? null)}
-</button>
+	<button class={['contents', className]} onclick={() => (open = true)}>
+		{@render children(selectedStation?.stationName ?? null, selectedStation?.crsCode ?? null)}
+	</button>
+</div>
