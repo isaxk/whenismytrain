2fc4a9abb7066d819b9ebc80600c876bc1aa8747
isaxk <76838845+isaxk@users.noreply.github.com>
2025-05-31 11:30:50 +0100

    cloudflare env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit(), cloudflare()]
 });
